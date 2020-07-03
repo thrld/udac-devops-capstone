466278d661558e041df74dcdfaf547ea1f84e6ad
Thomas Herold <thomas.herold@areto.de>
2020-07-03 15:11:04 +0200

    Update shell scripts for local testing

--- blue_green_static_html/blue/run_docker.sh
+++ blue_green_static_html/blue/run_docker.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-sudo docker build --tag=blueimage .
+docker build --tag=blueimage .
 
 # Step 2: 
 # List docker images
-sudo docker image ls
+docker image ls
 
 # Step 3: 
 # Run flask app
-sudo docker run -p 8000:80 blueimage
+docker run -p 8000:80 blueimage
--- blue_green_static_html/green/run_docker.sh
+++ blue_green_static_html/green/run_docker.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-sudo docker build --tag=greenimage .
+docker build --tag=greenimage .
 
 # Step 2: 
 # List docker images
-sudo docker image ls
+docker image ls
 
 # Step 3: 
 # Run flask app
-sudo docker run -p 8000:80 greenimage
+docker run -p 8000:80 greenimage

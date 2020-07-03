f2e2de1479c5a335ae5637c267277356f0ff4403
Thomas Herold <thomas.herold@areto.de>
2020-07-03 11:23:44 +0200

    Change .sh to sudo for simplicity

--- blue_green_static_html/blue/run_docker.sh
+++ blue_green_static_html/blue/run_docker.sh
@@ -4,12 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=blueimage .
+sudo docker build --tag=blueimage .
 
 # Step 2: 
 # List docker images
-docker image ls
+sudo docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 blueimage
+sudo docker run -p 8000:80 blueimage
--- blue_green_static_html/green/upload_docker.sh
+++ blue_green_static_html/green/upload_docker.sh
@@ -12,9 +12,9 @@ DOCKER_PATH=$DOCKER_USER/$IMAGE_NAME
 # Step 2:
 # Authenticate & tag
 echo "Docker ID and Image: $DOCKER_PATH"
-docker login --username $DOCKER_USER
-docker tag $IMAGE_NAME $DOCKER_PATH
+sudo docker login --username $DOCKER_USER
+sudo docker tag $IMAGE_NAME $DOCKER_PATH
 
 # Step 3:
 # Push image to a docker repository
-docker push $DOCKER_PATH
+sudo docker push $DOCKER_PATH
